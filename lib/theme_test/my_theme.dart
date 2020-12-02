dced303f7fb9720e398875fff91009f76ceeb3ec
Victor <victor950602@gmail.com>
2020-12-02 11:51:46 +0800

    v0.0.20201202.1

---
+++ lib/theme_test/my_theme.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+
+void main () {
+  runApp(MyThemePage());
+}
+
+
+class MyThemePage extends StatefulWidget {
+
+  MyThemePage({Key key}) : super(key: key);
+  _MyThemePageState createState ()=> _MyThemePageState();
+}
+
+class _MyThemePageState extends State<MyThemePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        scaffoldBackgroundColor: Color.fromRGBO(47, 49, 53, 1.000),
+        primaryColor: Colors.amber,
+        brightness: Brightness.dark,
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.indigo,
+          textTheme: ButtonTextTheme.primary,
+        ),
+        cardTheme: CardTheme(
+          color: Color.fromRGBO(53, 56, 60, 1.000),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          ),
+          elevation: 3.0,
+          shadowColor: Color.fromRGBO(0, 0, 0, 1.000),
+        )
+      ),
+      darkTheme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("This is theme"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton(onPressed: (){}, child: Text("click"),),
+              Card(
+                child: Container(
+                  width: 200,
+                  height: 200,
+                  child: Center(
+                    child: Text("This is a card"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
